97167c897a3ed27be531e5bb7d54e32353c71344
NevinGregory <nevin.gregory2022@gmail.com>
2023-10-13 19:47:15 -0400

    Connected backend to frontend

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -2,9 +2,21 @@ import { View, Text, StatusBar, StyleSheet, Button } from 'react-native'
 import React, { useState, useEffect } from 'react'
 
 const Home = ({navigation}) => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://128.61.63.216:8000/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => {
+        console.error(error);
+      })
+  }, []);
+  
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+      <Text>{message}</Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
