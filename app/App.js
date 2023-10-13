5ed44457833f0e9a4608ba218955d9941c8f8d99
Roland Saavedra <rolandsaavedra22@gmail.com>
2023-10-13 18:44:24 -0400

    Added native stack navigator

--- app/App.js
+++ app/App.js
@@ -1,10 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screens/Home';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Home/>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home}/>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
