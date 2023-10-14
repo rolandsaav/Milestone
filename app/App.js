f2fe2786b4616ea19962cf4293029c840991a0e2
Roland Saavedra <rolandsaavedra22@gmail.com>
2023-10-14 18:14:19 -0400

    Register and login screens work

--- app/App.js
+++ app/App.js
@@ -1,39 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, Touchable } from 'react-native';
 import MainContainer from './navigation/MainContainer';
-import Home from './navigation/screens/Home';
-import Goals from './navigation/screens/Goals';
-import Profile from './navigation/screens/Profile';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './navigation/screens/loginscreen';
 import AuthContainer from './navigation/AuthContainer';
+import React, { useState } from 'react';
+
+const AuthContext = React.createContext();
 
 export default function App() {
-  const user = null;
-  return (
-    <NavigationContainer>
-      {user != null ? <MainContainer/> : <AuthContainer/>}
-    </NavigationContainer>
-  );
-}
+  const [user, setUser] = useState(null);
 
-const styles = StyleSheet.create({
-  text: {
-    fontFamily: "Avenir Next",
-    color: "1D2029"
-  },
-  socialButton: {
-    flexDirection: "row",
-    marginHorizontal: 12,
-    paddingHorizontal: 30,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderColor: "rgba(171, 180, 189, 0.6)",
-    borderRadius: 4,
-    backgroundColor: "#fff",
-    shadowColor: "rgba(171, 180, 0.35)",
-    shadowOffset: {width: 0, height: 10},
-    shadowOpacity: 1,
-    shadowRadius: 20,
-    elevation: 5
-  }
-});
+  return (
+    <AuthContext.Provider value={[user, setUser]}>
+    <NavigationContainer>
+     {user == null ? <AuthContainer/> : <MainContainer/>}
+    </NavigationContainer>
+    </AuthContext.Provider>
+  );
+};
+export {AuthContext}
--- app/navigation/screens/loginscreen.js
+++ app/navigation/screens/loginscreen.js
@@ -1,46 +1,70 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, Touchable} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, Touchable } from 'react-native';
 import InputTextField from '../../components/InputTextField';
 import KeyboardAvoidingContainer from '../../components/KeyboardAvoidingContainer';
 import SocialButton from '../../components/SocialButton';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../App';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase';
 const facebookImg = require("../../assets/Facebook.png")
 const googleImg = require("../../assets/Google.png")
 
-const LoginScreen = () => {
-  return (
-    <KeyboardAvoidingContainer style={styles.container}>
-        <View>
-            <View style={{marginTop: 60, alignItems: "center", justifyContent: "center"}}>
-                <Image source = {require('../../assets/logo.png')} style={styles.mainLogo}></Image>
-            </View>
-            <View style={{marginTop: 48, flexDirection: "row", justifyContent: "center"}}>
-                <SocialButton name={"Facebook"} imageSource={facebookImg}/>
-                <SocialButton name={"Google"} imageSource={googleImg}/>
-            </View>
+const LoginScreen = ({ navigation }) => {
+    const [user, setUser] = useContext(AuthContext)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const onLogin = () => {
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCred) => {
+            const loggedIn = userCred.user;
+            setUser(loggedIn);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
 
-            <Text style={[styles.text, {color: "#ABB4BD", fontSize: 15, textAlign: "center", marginVertical: 20}]}
-            >
-                Or
-            </Text>
-        
-            <InputTextField title="Email"></InputTextField>
-            <InputTextField 
+    return (
+        <KeyboardAvoidingContainer style={styles.container}>
+            <View>
+                <View style={{ marginTop: 60, alignItems: "center", justifyContent: "center" }}>
+                    <Image source={require('../../assets/logo.png')} style={styles.mainLogo}></Image>
+                </View>
+                <View style={{ marginTop: 48, flexDirection: "row", justifyContent: "center" }}>
+                    <SocialButton name={"Facebook"} imageSource={facebookImg} />
+                    <SocialButton name={"Google"} imageSource={googleImg} />
+                </View>
+
+                <Text style={[styles.text, { color: "#ABB4BD", fontSize: 15, textAlign: "center", marginVertical: 20 }]}
+                >
+                    Or
+                </Text>
+
+                <InputTextField title="Email" onChange={e => {
+                console.log(e);
+                setEmail(e);
+            }} style={{}}></InputTextField>
+            <InputTextField onChange={e => {
+                console.log(e);
+                setPassword(e);
+            }}
                 style={{marginTop: 32, marginBottom: 8}} 
                 title="Password" 
                 isSecure={true}
             ></InputTextField>
 
-            <Text style={[styles.text, styles.link, {textAlign: "right"}]}>Forgot Password?</Text>
+                <Text style={[styles.text, styles.link, { textAlign: "right" }]}>Forgot Password?</Text>
 
-            <TouchableOpacity style={styles.submitContainer}>
-                <Text style={[styles.text, {color: "#fff", fontWeight: "600", fontSize: 16}]}>Login</Text>
-            </TouchableOpacity>
+                <TouchableOpacity style={styles.submitContainer}>
+                    <Text style={[styles.text, { color: "#fff", fontWeight: "600", fontSize: 16 }]} onPress={onLogin}>Login</Text>
+                </TouchableOpacity>
 
-            <Text style={[styles.text, {fontSize: 14, color: "#ABB4BD", textAlign: "center", marginTop: 24}]}>
-                Don't have an account? <Text style={[styles.text, styles.link]}>Register Now</Text>
-            </Text>
-        </View>
-    </KeyboardAvoidingContainer>
-  )
+                <Text style={[styles.text, { fontSize: 14, color: "#ABB4BD", textAlign: "center", marginTop: 24 }]} onPress={() => { navigation.navigate("Register") }}>
+                    Don't have an account? <Text style={[styles.text, styles.link]}>Register Now</Text>
+                </Text>
+            </View>
+        </KeyboardAvoidingContainer>
+    )
 }
 
 export default LoginScreen
@@ -52,23 +76,23 @@ const styles = StyleSheet.create({
         paddingHorizontal: 30
     },
     text: {
-      fontFamily: "Avenir Next",
-      color: "1D2029"
+        fontFamily: "Avenir Next",
+        color: "#1D2029"
     },
     socialButton: {
-      flexDirection: "row",
-      marginHorizontal: 12,
-      paddingVertical: 12,
-      paddingHorizontal: 30,
-      borderWidth: StyleSheet.hairlineWidth,
-      borderColor: "rgba(171, 180, 189, 0.65)",
-      borderRadius: 4,
-      backgroundColor: "#fff",
-      shadowColor: "rgba(171, 180, 0.35)",
-      shadowOffset: {width: 0, height: 5},
-      shadowOpacity: .5,
-      shadowRadius: 5,
-      elevation: 1
+        flexDirection: "row",
+        marginHorizontal: 12,
+        paddingVertical: 12,
+        paddingHorizontal: 30,
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: "rgba(171, 180, 189, 0.65)",
+        borderRadius: 4,
+        backgroundColor: "#fff",
+        shadowColor: "rgba(171, 180, 0.35)",
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: .5,
+        shadowRadius: 5,
+        elevation: 1
     },
     mainLogo: {
         width: 300,
@@ -94,8 +118,8 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
         shadowColor: "rgba(171, 180, 0.35)",
-        shadowOffset: {width: 0, height: 5},
+        shadowOffset: { width: 0, height: 5 },
         shadowOpacity: .5,
         shadowRadius: 5,
     }
-  });
+});
