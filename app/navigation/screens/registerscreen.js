5c74a23751e55725e1936eb0200aa7f591bc235e
Roland Saavedra <rolandsaavedra22@gmail.com>
2023-10-14 22:16:40 -0400

    refactored add user in regsiter screen component

--- app/navigation/screens/registerscreen.js
+++ app/navigation/screens/registerscreen.js
@@ -18,135 +18,82 @@ const RegisterScreen = ({ navigation }) => {
     const onRegister = () => {
         var uid;
         createUserWithEmailAndPassword(auth, email, password)
-        .then((userCred) => {
-            console.log(userCred.user);
-            const user = userCred.user;
-            setUser(user);
-            uid = user.uid;
-            addUser(uid, username, name, email);
-        })
-        .catch((error) => {
+            .then((userCred) => {
+                console.log(userCred.user);
+                const user = userCred.user;
+                setUser(user);
+                uid = user.uid;
+                addUser(uid, username, name, email);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+    const addUser = async (uid, username, name, email) => {
+        try {
+            const response = await fetch('http://128.61.63.216:8080/api/users', {
+                method: "POST",
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username: username, name: name, uid: uid, email: email }),
+            });
+        } catch (error) {
             console.error(error);
-        });        
+        }
     }
-  return (
-    <KeyboardAvoidingContainer style={styles.container}>
-        <View>
-            <View style={{ marginTop: 60, alignItems: "center", justifyContent: "center" }}>
-                <Image source={require('../../assets/logo.png')} style={styles.mainLogo}></Image>
+
+    return (
+        <KeyboardAvoidingContainer style={styles.container}>
+            <View>
+                <View style={{ marginTop: 60, alignItems: "center", justifyContent: "center" }}>
+                    <Image source={require('../../assets/logo.png')} style={styles.mainLogo}></Image>
+                </View>
+                <View style={{ marginTop: 48, flexDirection: "row", justifyContent: "center" }}>
+                    <SocialButton name={"Facebook"} imageSource={facebookImg} />
+                    <SocialButton name={"Google"} imageSource={googleImg} />
+                </View>
+
+                <Text style={[styles.text, { color: "#ABB4BD", fontSize: 15, textAlign: "center", marginVertical: 20 }]}
+                >
+                    Or
+                </Text>
+
+                <View style={{ gap: 20 }}>
+                    <InputTextField title="Name" onChange={e => {
+                        console.log(e);
+                        setName(e);
+                    }} style={{}}></InputTextField>
+                    <InputTextField title="Username" onChange={e => {
+                        console.log(e);
+                        setUsername(e);
+                    }} style={{}}></InputTextField>
+                    <InputTextField title="Email" onChange={e => {
+                        console.log(e);
+                        setEmail(e);
+                    }} style={{}}></InputTextField>
+                    <InputTextField onChange={e => {
+                        console.log(e);
+                        setPassword(e);
+                    }}
+                        title="Password"
+                        isSecure={true}
+                    ></InputTextField>
+                </View>
+
+
+                <TouchableOpacity style={styles.submitContainer} onPress={onRegister}>
+                    <Text style={[styles.text, { color: "#fff", fontWeight: "600", fontSize: 16 }]}>Register</Text>
+                </TouchableOpacity>
+
+                <Text style={[styles.text, { fontSize: 14, color: "#ABB4BD", textAlign: "center", marginTop: 24 }]}>
+                    Already have an account? <Text style={[styles.text, styles.link]} onPress={() => { navigation.navigate("Login") }}>Login Now</Text>
+                </Text>
             </View>
-            <View style={{ marginTop: 48, flexDirection: "row", justifyContent: "center" }}>
-                <SocialButton name={"Facebook"} imageSource={facebookImg} />
-                <SocialButton name={"Google"} imageSource={googleImg} />
-            </View>
-
-            <Text style={[styles.text, { color: "#ABB4BD", fontSize: 15, textAlign: "center", marginVertical: 20 }]}
-            >
-                Or
-            </Text>
-
-            <View style={{gap: 20}}>
-            <InputTextField title="Name" onChange={e => {
-                console.log(e);
-                setName(e);
-            }} style={{}}></InputTextField>
-            <InputTextField title="Username" onChange={e => {
-                console.log(e);
-                setUsername(e);
-            }} style={{}}></InputTextField>
-            <InputTextField title="Email" onChange={e => {
-                console.log(e);
-                setEmail(e);
-            }} style={{}}></InputTextField>
-            <InputTextField onChange={e => {
-                console.log(e);
-                setPassword(e);
-            }}
-                title="Password"
-                isSecure={true}
-            ></InputTextField>
-            </View>
+        </KeyboardAvoidingContainer>
+    )
+};
 
 
-            <TouchableOpacity style={styles.submitContainer} onPress={onRegister}>
-                <Text style={[styles.text, { color: "#fff", fontWeight: "600", fontSize: 16 }]}>Register</Text>
-            </TouchableOpacity>
-
-            <Text style={[styles.text, { fontSize: 14, color: "#ABB4BD", textAlign: "center", marginTop: 24 }]}>
-                Already have an account? <Text style={[styles.text, styles.link]} onPress={() => { navigation.navigate("Login") }}>Login Now</Text>
-            </Text>
-        </View>
-    </KeyboardAvoidingContainer>
-  )
-  
-async function addUser(uid = '', username = '', name = '', email = '') {
-    try { 
-        const response = await fetch('http://128.61.63.216:8080/api/users', {
-            method: "POST",
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({username: username, name: name, uid: uid, email: email}),
-        });
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        felx: 1,
-        backgroundColor: "#fff",
-        paddingHorizontal: 30
-    },
-    text: {
-        fontFamily: "Avenir Next",
-        color: "#1D2029"
-    },
-    socialButton: {
-        flexDirection: "row",
-        marginHorizontal: 12,
-        paddingVertical: 12,
-        paddingHorizontal: 30,
-        borderWidth: StyleSheet.hairlineWidth,
-        borderColor: "rgba(171, 180, 189, 0.65)",
-        borderRadius: 4,
-        backgroundColor: "#fff",
-        shadowColor: "rgba(171, 180, 0.35)",
-        shadowOffset: { width: 0, height: 5 },
-        shadowOpacity: .5,
-        shadowRadius: 5,
-        elevation: 1
-    },
-    mainLogo: {
-        width: 300,
-        height: 200,
-        marginRight: 8,
-    },
-    socialLogo: {
-        width: 16,
-        height: 16,
-        marginRight: 8,
-    },
-    link: {
-        color: "#17b978",
-        fontSize: 14,
-        fontWeight: "500"
-    },
-    submitContainer: {
-        backgroundColor: "#17b978",
-        fontSize: 16,
-        borderRadius: 4,
-        paddingVertical: 12,
-        marginTop: 32,
-        alignItems: "center",
-        justifyContent: "center",
-        shadowColor: "rgba(171, 180, 0.35)",
-        shadowOffset: { width: 0, height: 5 },
-        shadowOpacity: .5,
-        shadowRadius: 5,
-    }
-});
-
-export default RegisterScreen
+export default RegisterScreen;
