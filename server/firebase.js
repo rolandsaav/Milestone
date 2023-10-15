8de69d81ddf8a2aadfb3f286cccef1ac7bb68f42
NevinGregory <nevin.gregory2022@gmail.com>
2023-10-14 20:49:03 -0400

    Connected frontend to backend to database for POST

--- server/firebase.js
+++ server/firebase.js
@@ -1,5 +1,6 @@
 const firestore = require("@google-cloud/firestore")
 const key = require("./service-key.json")
+
 const db = new firestore.Firestore({
   projectId: "milestone-401923",
   keyFilename: "./service-key.json",
